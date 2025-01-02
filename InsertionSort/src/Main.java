9b0241b2982243bbf440fe03381739954db415fc
KDeepak5673 <deepakchandrashekhar0309@gmail.com>
2025-01-02 21:43:50 +0530

    insertion and cyclic sort

---
+++ InsertionSort/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        //we use insertion sort because
+
+        // it is adaptive :steps get reduced if array is sorted. no of swaps reduuced as copmaired to bubble sort
+        // Stable
+        // Use for smaller value of N
+        int[] arr = {3,1,5,4 ,2};
+
+        insertion(arr);
+
+        System.out.println(Arrays.toString(arr));
+        
+
+    }
+
+    static void insertion(int[] arr){
+        for (int i = 0; i < arr.length-1; i++) {
+
+            for (int j = i+1; j > 0; j--) {
+                if(arr[j] < arr[j-1]){
+                    swap(arr , j , j-1);
+                }else {
+                    break;
+                }
+            }
+            
+        }
+    }
+
+    static void swap(int[] arr, int first , int second){
+
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+
+    }
+}
