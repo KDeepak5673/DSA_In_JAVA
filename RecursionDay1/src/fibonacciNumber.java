610b3ab98b75e1bc836735eaf6177ac6c550e27a
KDeepak5673 <deepakchandrashekhar0309@gmail.com>
2025-01-12 20:20:54 +0530

    Recursion

---
+++ RecursionDay1/src/fibonacciNumber.java
@@ -0,0 +1,19 @@
+public class fibonacciNumber {
+    public static void main(String[] args) {
+
+        int ans = fibonacci(4);
+
+        System.out.println(ans);
+
+    }
+
+    static int fibonacci(int n ){
+
+        if(n < 2){
+            return n;
+        }
+
+
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
---
+++ RecursionDay1/src/message.java
@@ -0,0 +1,25 @@
+
+public class message {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        message();
+    }
+    static void message(){
+        System.out.println("Hello world!");
+
+        mesasge1();
+    }
+
+    private static void mesasge1() {
+        System.out.println("Hello world!");
+
+        message2();
+    }
+
+    private static void message2() {
+
+        System.out.println("Hello world!");
+
+    }
+}
