dada7a45005cd9ad3d2afaeae7fdbb6f73fb73cb
KDeepak5673 <deepakchandrashekhar0309@gmail.com>
2025-02-07 15:33:35 +0530

    Binary Treee

---
+++ Tree1_BST/src/MainBinaryTree.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class MainBinaryTree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        binaryTree tree = new binaryTree();
+        tree.populate(scanner);
+//        tree.display();
+        tree.preetyDisplay();
+    }
+}
